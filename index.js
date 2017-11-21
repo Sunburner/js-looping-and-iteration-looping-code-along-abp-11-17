9444f8efda3b73e4177ff06bbc1369755ed829c6
Angelo Cigna <acigna1982@gmail.com>
2017-11-21 18:56:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(arrayNames) {
+  for(let i = 0; i < arrayNames.length; i++) {
+    arrayNames[i]
+    console.log(`Welcome ${arrayNames[i]}! You are employee #${i+1}.`)
+  }
+  return arrayNames
+}
+
+function cointoss() {
+  return Math.random >= .5;
+
+}
+
+function tailsNeverFails() {
+  let counter = 0;
+  while(cointoss()) {
+    counter++
+return (`You got ${counter} tails in a row!`)
+}
+}
